fc114000198b4248ce80cc8f98c5bf1774ef5535
saurabhburade <sburade3@gmail.com>
2022-10-11 21:37:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sample-subgraph
+# sample-subgraph
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,36 +1,38 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { ApprovalSchema } from "./../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   UChildERC20Proxy,
   Approval,
   OwnershipTransferred,
-  Transfer
-} from "../generated/UChildERC20Proxy/UChildERC20Proxy"
-import { ExampleEntity } from "../generated/schema"
+  Transfer,
+} from "../generated/UChildERC20Proxy/UChildERC20Proxy";
 
 export function handleApproval(event: Approval): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let approvalEntity = ApprovalSchema.load(event.transaction.from.toHex());
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
+  if (approvalEntity == null) {
+    approvalEntity = new ApprovalSchema(event.transaction.from.toHex());
+    approvalEntity.spender = event.params.spender;
+    approvalEntity.value = event.params.value;
+    approvalEntity.amount = event.params.value;
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    // entity.count = BigInt.fromI32(0);
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // entity.count = entity.count + BigInt.fromI32(1);
 
   // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
+  // entity.owner = event.params.owner;
+  // entity.spender = event.params.spender;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
-
+  // entity.save();
+  approvalEntity.save();
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
   // `new Entity(...)`, set the fields that should be updated and save the
